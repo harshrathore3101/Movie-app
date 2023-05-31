34d161cc1a78b320b8a747623b65882fbdd588ad
Harsh Rathore <85802496+harshrathore3101@users.noreply.github.com>
2023-05-31 12:06:21 +0530

    Update fetchApi.js

--- src/Utils/fetchApi.js
+++ src/Utils/fetchApi.js
@@ -5,5 +5,5 @@ const BaseURL = "https://api.themoviedb.org/3/movie/";
 export const fetchFromApi = async (url) => {
   const { data } = await axios.get(`${BaseURL}/${url}`);
 
-  return data;
+  return data; 
 };
